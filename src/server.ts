c5a63c84d797d16dacd640548eece12dfde19ca2
Pascual Herrera M. <ing_herrera16@hotmail.com>
2026-02-27 17:50:27 -0600

    Agregar ruta '/api/' para probar que el Servidor Funciona

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express";
 import projectRoutes from "./routes/projectRoutes";
 import authRoutes from "./routes/authRoutes";
 import utilsRoutes from "./routes/utilsRoutes";
@@ -6,15 +6,16 @@ import utilsRoutes from "./routes/utilsRoutes";
 import cors from "cors";
 import { corsConfig } from "./config/cors";
 
-
 const app = express();
 app.use(cors(corsConfig));
 
-
 // Leer datos de formularios
-app.use(express.json())
+app.use(express.json());
 
 //Rutas
+app.get("/api/", (req: Request, res: Response) => {
+	res.json({ mensaje: "Bienvenido a la API de UpTask" });
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/utils", utilsRoutes);
