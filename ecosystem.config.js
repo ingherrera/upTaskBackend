613f55e7d34234ff020f93cc4f93710c723f821e
Pascual Herrera M. <ing_herrera16@hotmail.com>
2026-03-11 17:11:14 -0600

    Agregue ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "uptask-backend",
+      script: "index.js",
+      cwd: "/var/www/upTaskBackend/current",
+      watch: false, // Cámbialo a true si quieres que reinicie al detectar cambios
+      time: true,
+      env: {
+        NODE_ENV: "production",
+        PORT: 4000,
+        DATABASE_URL: "mysql://uptask-user:Pascual71@localhost:3306/uptask",
+        // FRONTEND_URL:"http://localhost:3000",
+        FRONTEND_URL: "http://192.168.122.155:3000",
+        FRONTEND_URL1: "http://localhost:8000",
+        JWT_SECRET: "palabraUltraRecontraSecreta",
+      },
+    },
+  ],
+};
