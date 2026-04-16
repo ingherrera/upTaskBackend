1d240f57e02c6e610a97198b43438194fdf91059
Pascual Herrera M. <ing_herrera16@hotmail.com>
2026-04-16 14:03:51 -0600

    ecosystem - ya no se buildea

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,9 @@ module.exports = {
   apps: [
     {
       name: "uptask-backend",
-      script: "dist/index.js",
+      // script: "dist/index.js",
+      script: "node_modules/.bin/tsx", // para no buildear en cada modificacion
+      args: "src/index.ts", // iguL
       cwd: "/var/www/upTaskBackend",
       watch: false,
       time: true,
