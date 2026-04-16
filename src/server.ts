a16daff34eba216662d79fd0386fe7766de67194
Pascual Herrera M. <ing_herrera16@hotmail.com>
2026-04-16 14:24:36 -0600

    Modificacion corta de prueba

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 //Rutas
 app.get("/api/", (req: Request, res: Response) => {
   res.json({
-    mensaje: "UpTask API v5 - Despliegue solo cuando decida hacerlo -- ",
+    mensaje: "UpTask API v6 - Despliegue solo cuando decida hacerlo -- ",
   });
 });
 app.use("/api/auth", authRoutes);
